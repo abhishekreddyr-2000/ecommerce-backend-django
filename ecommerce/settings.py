0dd5cba6355e79e1782aafb7df712ac9aa8dfa4a
abhishekreddyr-2000 <abhishekreddyr1000@gmail.com>
2026-08-31 16:49:04 +0530

    Move secret key to environment variable

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,18 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q9iv_@cibb@!6p&bxj!y(#x%a_u)*(0c*zws=iihds)m&*u%4*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
